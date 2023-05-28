aca5be78eee9c75fd6a0015f724c8be6e853507b
Noam Musba <noamm7466@gmail.com>
2023-05-28 17:10:31 +0300

    Update FavPok.js
    added an image of pokeball, that when you click it, you go over my favorite pokemon

--- src/components/FavPok.js
+++ src/components/FavPok.js
@@ -2,17 +2,24 @@ import React from "react";
 
 function FavPok() {
   return (
-    <div style={{paddingTop: '70px', paddingBottom: '30px'}}>
-      <h2 style={{backgroundColor: 'pink'}}>Do you want to see and learn about my favorite pokemon?</h2>
-      <h3 style={{backgroundColor: 'yellow'}}>
+    <div style={{ paddingTop: "70px", paddingBottom: "30px" }}>
+      <h2 style={{ backgroundColor: "pink" }}>
+        Do you want to see and learn about my favorite pokemon?
+      </h2>
+      <h3 style={{ backgroundColor: "yellow" }}>
         Click{" "}
         <a
           href="https://www.pokemon.com/us/pokedex/espeon"
           title="go see the cute espeon!"
           target="_blank"
         >
-          here
-        </a>{" "}
+          <img
+            style={{ maxWidth: "8%" }}
+            src="https://e7.pngegg.com/pngimages/607/890/png-clipart-pokeball-pokeball-thumbnail.png"
+            alt="pokeball"
+          />
+        </a>
+        {" "}
         to see it!
       </h3>
     </div>
