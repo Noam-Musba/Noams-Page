d77540ee3545316f3af3f7e3ad5a2cb40db25437
Noam Musba <noam@dazl.dev>
2026-08-24 14:59:43 +0300

    feat: dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import styles from "./App.module.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Contact from "./components/Contact";
+import { useTheme } from "./hooks/useTheme";
 
 function App() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <>
       <a className={styles.skipLink} href="#main-content">
         Skip to main content
       </a>
-      <Header />
+      <Header theme={theme} onThemeChange={setTheme} />
       <Main />
       <Contact />
     </>
